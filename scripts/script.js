7aa6d96995ae3a54a06cf2281c320ce50fc05532
Raghunath javare <raghunath.javare@gmail.com>
2023-05-09 10:43:00 +0530

    add a js funtionality to my aapp

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const currentTime = document.querySelector(".clock__time");
+
+// timer
+
+(function () {
+  setInterval(function () {
+    const time = new Date();
+    const hour = String(time.getHours()).padStart(2, 0);
+    const min = String(time.getMinutes()).padStart(2, 0);
+    const sec = String(time.getSeconds()).padStart(2, 0);
+    currentTime.textContent = `${hour}:${min}:${sec}`;
+  }, 1000);
+})();
+
+
+    
