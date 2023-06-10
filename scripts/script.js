8c8b6fa2e7b4fa1845d81462df3948280c96993c
Raghunath javare <raghunath.javare@gmail.com>
2023-06-10 10:51:03 +0530

    add new icons to my app and add js funtionality to my app

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
+"use strick";
+
 const currentTime = document.querySelector(".clock__time");
+const iconContainer = document.querySelector(".app__icons");
+const content = document.querySelectorAll(".content");
+
+iconContainer.addEventListener("click", function (e) {
+  const event = e.target;
+
+  if (event.classList.contains("icons")) {
+    content.forEach((c) => c.classList.add("unactive--content"));
+    document
+      .querySelector(`.app__content--${event.dataset.tab}`)
+      .classList.remove("unactive--content");
+  }
+});
 
 // timer
+
+
+
+
 
 (function () {
   setInterval(function () {
@@ -11,6 +30,3 @@ const currentTime = document.querySelector(".clock__time");
     currentTime.textContent = `${hour}:${min}:${sec}`;
   }, 1000);
 })();
-
-
-
